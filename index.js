c1d599718bf9877b61a782c5a71b22be40489138
1000ch <shogo.sensui@gmail.com>
2016-07-24 17:41:56 +0900

    Use exec-buffer

--- index.js
+++ index.js
@@ -4,56 +4,44 @@ const fs = require('fs');
 const path = require('path');
 const gutil = require('gulp-util');
 const through = require('through2');
-const tempfile = require('tempfile');
-const rimraf = require('rimraf');
-const execFile = require('child_process').execFile;
+const execBuffer = require('exec-buffer');
 const cwebp = require('cwebp-bin');
 
-module.exports = function(options) {
-  options = options ? options : {};
+module.exports = options => through.obj(function(file, encode, callback) {
+  if (file.isNull()) {
+    this.push(file);
+    return callback();
+  }
 
-  return through.obj(function(file, encode, callback) {
-    if (file.isNull()) {
-      this.push(file);
-      return callback();
-    }
+  if (file.isStream()) {
+    this.emit('error', new gutil.PluginError('gulp-cwebp', 'Streaming not supported'));
+    return callback();
+  }
 
-    if (file.isStream()) {
-      this.emit('error', new gutil.PluginError('gulp-cwebp', 'Streaming not supported'));
-      return callback();
-    }
+  const extension = path.extname(file.path).toLowerCase();
 
-    const extension = path.extname(file.path).toLowerCase();
+  if (['.jpg', '.jpeg', '.png'].indexOf(extension) === -1) {
+    gutil.log('gulp-cwebp: Skipping unsupported image ' + gutil.colors.blue(file.relative));
+    return callback();
+  }
 
-    if (['.jpg', '.jpeg', '.png'].indexOf(extension) === -1) {
-      gutil.log('gulp-cwebp: Skipping unsupported image ' + gutil.colors.blue(file.relative));
-      return callback();
-    }
-
-    const dest = tempfile();
-    const args = [file.path, '-o', dest];
-
-    Object.keys(options).forEach(key => {
-      args.push(`-${key}`);
-      args.push(options[key]);
-    });
-
-    execFile(cwebp, args, error => {
-      if (error) {
-        return callback(new gutil.PluginError('gulp-cwebp', error));
-      }
-
-      file.contents = fs.readFileSync(dest);
-      file.path = gutil.replaceExtension(file.path, '.webp');
-      this.push(file);
-
-      rimraf(dest, error => {
-        if (error) {
-          return callback(new gutil.PluginError('gulp-cwebp', error));
-        }
-
-        callback();
-      });
-    });
+  const args = [];
+  Object.keys(Object.assign({}, options)).forEach(key => {
+    args.push(`-${key}`);
+    args.push(options[key]);
   });
-};
+  args.push('-o', execBuffer.output, execBuffer.input);
+
+  execBuffer({
+    input : file.contents,
+    bin   : cwebp,
+    args  : args
+  }).then(buffer => {
+    file.contents = buffer;
+    file.path = gutil.replaceExtension(file.path, '.webp');
+    this.push(file);
+    callback();
+  }).catch(error => {
+    callback(new gutil.PluginError('gulp-cwebp', error));
+  });
+});
